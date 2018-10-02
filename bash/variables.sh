3b89614de560a243ec33d66d93d9d03724de3509
Daniel Stevenson <daniel.stevenson@live.ca>
2018-10-02 15:54:38 -0400

    Changed to include random title with cowsay

--- bash/variables.sh
+++ bash/variables.sh
@@ -9,8 +9,11 @@ today=$(date +%A)
 titles=("All" "I" "Do" "Is" "Win")
 title_index=$((RANDOM % ${#titles[@]}))
 title=${titles[$title_index]}
+welcome_message="Welcome to planet $hostname, $title $myname!
+Today is $today."
 
 echo "Welcome to planet $hostname, $mytitle $myname!"
 echo "Today is $today."
 
 echo $title
+echo $welcome_message | cowsay -f dragon
