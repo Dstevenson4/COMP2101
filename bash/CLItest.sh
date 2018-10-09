192d7b7aaef404eb760aba06084bb3d3519ba01d
Daniel Stevenson <daniel.stevenson@live.ca>
2018-10-09 16:48:18 -0400

    Changed cli program

--- bash/CLItest.sh
+++ bash/CLItest.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-# This script was made to test printing command line arguments
+# Shifting with command line
 
-echo $0 $1 $2 $3
-echo "Script finished"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help)
+      echo "Usage: $0 [-h|--help] [-d N|--debug N]"
+      exit 0
+      ;;
+    -d|--debug)
+      debug="$2"
+      shift
+      if [ -z "$debug" ]; then
+        echo "You have to give me a debug number if you use the debug option" >&2
+        echo "Usage: $0 [-h|--help] [-d N|--debug N]"
+        exit 2
+      fi
+      ;;
+    *)
+      echo "I didn't understand '$1'" >&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+
+echo "Done processing command line."
